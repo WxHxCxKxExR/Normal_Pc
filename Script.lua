b405846fa6b349c5686fa8328041268f3e3141d9
normalhubv1 <108181477+normalhubv1@users.noreply.github.com>
2023-03-25 00:30:04 +0700

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/BloxFruit.lua"), true))();
+elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 or game.PlaceId == 6596144663 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/KingLegacye.lua"), true))();
+elseif game.PlaceId == 4996049426 or game.PlaceId == 7785334488 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/AllStar.lua"), true))();
+elseif game.PlaceId == 3978370137 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/GPO.lua"), true))();
+elseif game.PlaceId == 9542890083 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/SeaDestiny.lua"), true))()
+else
+    game.Players.LocalPlayer:Kick("Not Support Game")
+end
