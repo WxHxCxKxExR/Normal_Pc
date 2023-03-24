29e35252e953ae6709d9de7335c86bbe49e562cb
normalhubv1 <108181477+normalhubv1@users.noreply.github.com>
2023-03-25 00:33:22 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,13 +1,9 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/BloxFruit.lua"), true))();
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Pc/main/BF-PC.lua"), true))();
 elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 or game.PlaceId == 6596144663 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/KingLegacye.lua"), true))();
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Pc/main/KL-PC.lua"), true))();
 elseif game.PlaceId == 4996049426 or game.PlaceId == 7785334488 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/AllStar.lua"), true))();
-elseif game.PlaceId == 3978370137 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/GPO.lua"), true))();
-elseif game.PlaceId == 9542890083 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub/main/SeaDestiny.lua"), true))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Pc/main/AST-PC.lua"), true))();
 else
-    game.Players.LocalPlayer:Kick("Not Support Game")
+    game.Players.LocalPlayer:Kick("Normal Hub :: Not Support This Game")
 end
