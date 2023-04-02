03856a02c2b5a107b0af1b033618e0721e6dade9
normalhubv1 <128838097+WxHxCxKxExR@users.noreply.github.com>
2023-04-02 18:16:46 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId 
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Pc/main/KL-PC.lua"), true))();
 elseif game.PlaceId == 4996049426 or game.PlaceId == 7785334488 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Pc/main/AST-PC.lua"), true))();
+elseif game.PlaceId == 6299805723 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Mb/main/AFS_PC.lua"), true))();
 else
     game.Players.LocalPlayer:Kick("Normal Hub :: Not Support This Game")
 end
